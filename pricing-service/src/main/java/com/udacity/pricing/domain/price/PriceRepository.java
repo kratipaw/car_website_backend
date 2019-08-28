aee5f1bd1236d4f7222957a34acfb7b53cec85be
kratipaw <kratipaw3@gmail.com>
2019-08-28 09:57:41 +0530

    Removed custom URL for pricing service and added some demo data for prices

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/PriceRepository.java
@@ -1,9 +1,7 @@
 package com.udacity.pricing.domain.price;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PriceRepository extends CrudRepository<Price, Long> {
 
 }
