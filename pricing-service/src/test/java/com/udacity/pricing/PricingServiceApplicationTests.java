72d81b025670d9b88b10e72482b60406ec31d887
kratipaw <kratipaw3@gmail.com>
2019-08-28 09:58:30 +0530

    Added pricing-service URl test case

--- pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -5,8 +5,11 @@ import com.udacity.pricing.domain.price.PriceRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 import java.util.Optional;
@@ -14,12 +17,19 @@ import java.util.Optional;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class PricingServiceApplicationTests {
 
 	@Autowired
 	PriceRepository priceRepository;
+
+	@Autowired
+	MockMvc mvc;
 
 	@Test
 	public void contextLoads() {
@@ -27,9 +37,17 @@ public class PricingServiceApplicationTests {
 	}
 
 	@Test
+	public void findPrice() throws Exception {
+		mvc.perform(
+				get("/prices/1")
+						.accept(MediaType.APPLICATION_JSON_UTF8))
+				.andExpect(status().isOk());
+	}
+
+	@Test
 	public void findAll(){
 		List<Price> prices = (List<Price>) priceRepository.findAll();
-		assertEquals(2, prices.size());
+		assertEquals(4, prices.size());
 		assertEquals(1, prices.get(0).getVehicleId().intValue());
 		assertEquals(2, prices.get(1).getVehicleId().intValue());
 	}
